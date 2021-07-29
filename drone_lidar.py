4108a4d1ea4cc6532a0abbba13978e4b9b4d9ff4
tmddn833 <tmddn833@korea.ac.kr>
2021-07-29 14:25:48 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	MavLinkCom/mavlink/mavlink_sha256.h
    #	Unreal/Plugins/AirSim/Content/AirSimAssets.umap

--- drone_lidar.py
+++ drone_lidar.py
@@ -1,4 +1,4 @@
-# Python cl ient example to get Lidardata from a drone
+# Python client example to get Lidar data from a drone
 #
 
 import setup_path 
--- reset_test_drone.py
+++ reset_test_drone.py
@@ -9,15 +9,15 @@ client.confirmConnection()
 client.enableApiControl(True)
 client.armDisarm(True)
 
-# print("fly")
-# client.moveToPositionAsync(0, 0, -10, 5).join()
+print("fly")
+client.moveToPositionAsync(0, 0, -10, 5).join()
 
 print("reset")
 client.reset()
 client.enableApiControl(True)
 client.armDisarm(True)
-#time.sleep(5)
+time.sleep(5)
 print("done")
-#
-# print("fly")
-# client.moveToPositionAsync(0, 0, -10, 5).join()
+
+print("fly")
+client.moveToPositionAsync(0, 0, -10, 5).join()
