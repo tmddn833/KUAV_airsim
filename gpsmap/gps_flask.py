78246236b6afa882324fb14c6b43aa539733d0a5
seungwoo <tmddn833@korea.ac.kr>
2021-08-02 05:35:25 +0900

    double gps

--- gpsmap/gps_flask.py
+++ gpsmap/gps_flask.py
@@ -1,26 +1,29 @@
-from flask import Flask, Response , render_template , jsonify
+from flask import Flask, Response, Request
 import time
+from flask_cors import CORS
+import threading
+import logging
+
 app = Flask(__name__)
+# app.logger.disabled = True
+# log = logging.getLogger('werkzeug')
+# log.disabled = True
+CORS(app)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
-
-global i
-i = 0
-@app.route('/gen',methods=['POST'])
-def gen():
-    global i
-    i+=1
-    if i % 3== 0:
-        i -=3
-    print(i)
-    lat_lon = [[79.40124108864444, 21.97974628259094],[80.40124108864444, 20.97974628259094],[81.40124108864444, 19.97974628259094]]
-    return jsonify({
-        'time' : lat_lon[i][0],})
-
-
+    lat_lon = [[79.40124108864444, 21.97974628259094], [80.40124108864444, 20.97974628259094],
+               [81.40124108864444, 19.97974628259094]]
+    i = int(time.time() * 10) % 3
+    return '{"geometry": {"type": "Point", "coordinates": ' + str(
+        lat_lon[i]) + '}, "type": "Feature", "properties": {}}'
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', debug=True)
+    thread = threading.Thread(target=app.run, kwargs={'host': '127.0.0.1', 'threaded': True, 'debug': True,
+                                                      'use_reloader':False}, daemon=True)
+    thread.start()
+    print('test')
+    while True:
+        print("it's working!")
+        time.sleep(1)
